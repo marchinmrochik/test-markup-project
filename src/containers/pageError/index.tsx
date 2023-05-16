4c41fbf59d868bd572cd3b212fe473f29efac36f
marchinmrochik <kiselroman280@gmail.com>
2023-05-16 04:22:20 +0300

    fix text

--- src/containers/pageError/index.tsx
+++ src/containers/pageError/index.tsx
@@ -9,7 +9,7 @@ export const PageErrorContainer = () => {
             <h2 className="error-page__title">404: Page Not Found</h2>
             <Timer duration={DELAY_REDIRECT} redirectUrl={ROUTER.HOME} />
             <p className="error-page__description">
-                Click on the logo in the header or you will be automatically redirected not to the home page
+                Click on the logo in the header or you will be automatically redirected to the home page
             </p>
         </div>
     );
