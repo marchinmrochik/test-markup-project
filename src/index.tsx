280ebd01c505b6cec6cc814f7ed29d63dd8519ba
marchinmrochik <kiselroman280@gmail.com>
2023-05-16 04:40:12 +0300

    added fix text position and HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
-import { Provider } from 'react-redux';
+import {HashRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import {store} from './store';
 
 const root = ReactDOM.createRoot(
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 );
