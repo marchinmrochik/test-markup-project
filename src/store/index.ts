f48791c28def68165a839e3b0cec35869ac5f01c
marchinmrochik <kiselroman280@gmail.com>
2023-05-16 12:32:58 +0300

    removed redux-thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cardsReducer from './reducers/cardsReducer';
-import thunk from 'redux-thunk';
 
 export const store = configureStore({
     reducer: {
         cards: cardsReducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
